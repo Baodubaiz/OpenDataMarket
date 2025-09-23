7551c9bb9bfa3373dffa0433d62ec04e376d5024
LamBao <baolam12122004@gmail.com>
2025-09-23 20:45:36 +0700

    feat(auth): implement user login and register

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import loginRouter from './routes/login.route';
+import register from './routes/register.route';
+import cors from 'cors';
+
 
 
 const app = express();
 const port = 3001;
+app.use(cors({
+    origin: 'http://localhost:3000', // 👈 Cho phép frontend truy cập
+    credentials: true,               // 👈 Nếu bạn dùng cookie thì bật cái này
+}));
+
+app.use(express.json());
+app.use('/', loginRouter);
+app.use('/', register);
+
 
 app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
